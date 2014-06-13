14c44d1ce0a52d8f5c01ed9c374757a8bfc9570b
Lance Johnson <lancejjohnson@gmail.com>
2014-06-13 07:57:39 -0400

    Mostly static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,12 +6,34 @@ describe "StaticPages" do
       visit '/static_pages/home'
       expect(page).to have_content('Microposts')
     end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Microposts | Home")
+    end
   end
 
   describe "Help Page" do
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Microposts | Help")
+    end
+  end
+
+  describe "About Page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Microposts | About")
     end
   end
 end
