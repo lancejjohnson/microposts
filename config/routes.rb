6198fbc5eeda7d3ba2c9ca393f7b91008ccff090
Lance Johnson <lancejjohnson@gmail.com>
2014-06-20 11:33:50 -0400

    Steps on user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Microposts::Application.routes.draw do
-  get "users/new"
+  # get "users/new"
   root 'static_pages#home'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/signup', to: 'users#new', via: 'get'
+
+  resources :users
 end
