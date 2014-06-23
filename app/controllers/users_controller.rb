bdd857bed2d7b8c8c77251449c4df7c7f2362b2b
Lance Johnson <lancejjohnson@gmail.com>
2014-06-23 08:50:57 -0400

    Functional signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,28 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    # Rails uses the 'name=' values in the html to determine the params
+    # applicable to a user.
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to Microposts!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user)
+      .permit(:name, :email, :password, :password_confirmation)
   end
 end
